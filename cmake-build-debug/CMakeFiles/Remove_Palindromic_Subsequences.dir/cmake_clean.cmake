296c69638e7e22b6e270b921cd126b99b2f3422b
Alex Alcazar <alexhalcazar369@gmail.com>
2022-08-02 11:53:37 -0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Remove_Palindromic_Subsequences.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Remove_Palindromic_Subsequences.dir/main.cpp.o"
+  "Remove_Palindromic_Subsequences"
+  "Remove_Palindromic_Subsequences.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Remove_Palindromic_Subsequences.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
